a881373247eea406a3386e019c82e133702c6d08
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-03-08 16:48:34 +0000

    Updated styles for Events and Event? EventList components

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,16 +1,16 @@
-function Event({ name, img, description, date, place }) {
+function Event({ name, img, description, date, place, eventURL }) {
 
   // const dateFormatted = Date(date)
   return (
     <div class="eventItem">
       <div class="eventImg">
-        <img src={img} alt="" />
+        <img src="https://github.com/NastasiaKarpenko/my-eire-app/blob/6b56bf30aa231e54ba38fe3463c543f2b079b763/src/images/events/Forest%20Fest%202024%20.jpeg?raw=true" alt="events_picture" />
       </div>
       <div class="eventMain">
         <div class="eventDate">{date}</div>
         <div class="eventTitle">{name}</div>
         <div class="eventDesc">{description}</div>
-        <button class="eventMore"> MORE </button>
+        <a href={eventURL} class="eventMore" target="_blank" rel="noopener noreferrer"> More details </a>
       </div>
       <div class="eventLocation">{place}</div>
     </div>
--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -4,41 +4,41 @@ import { getData } from "../db/firebase";
 
 const EventsList = () => {
   // TODO: uncomment this!!!
-  // const [events, setEvents] = useState([])
-  // useEffect(() => {
-  //   getData().then(result => setEvents(result))
-  // }, [])
+  const [events, setEvents] = useState([])
+  useEffect(() => {
+    getData().then(result => setEvents(result))
+  }, [])
 
-  // console.log(data)
+  console.log(events)
 
-  const events = [
-    {
-      name: "Claregalway Drama Festival 2024",
-      date: "14 - 22 Mar, 2024",
-      description:
-        "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-      place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-    },
-    {
-      name: "St. Patrick’s Day Festival 2024",
-      date: "14 - 22 Mar, 2024",
-      description:
-        "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-      place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-    },
-    {
-      name: "Múscailt – Annual Arts Festival",
-      date: "14 - 22 Mar, 2024",
-      description:
-        "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-      place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-    },
-  ];
+  // const events = [
+  //   {
+  //     name: "Claregalway Drama Festival 2024",
+  //     date: "14 - 22 Mar, 2024",
+  //     description:
+  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
+  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
+  //   },
+  //   {
+  //     name: "St. Patrick’s Day Festival 2024",
+  //     date: "14 - 22 Mar, 2024",
+  //     description:
+  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
+  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
+  //   },
+  //   {
+  //     name: "Múscailt – Annual Arts Festival",
+  //     date: "14 - 22 Mar, 2024",
+  //     description:
+  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
+  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
+  //   },
+  // ];
 
   return (
     <>
     <div className="eventContainer">
-        <Event
+        {/* <Event
           name={"NEW event"}
           description={
             "khsadkajs hdkjahd kajhsdkjahsk djhaskdjhaskjdha skjhaskdjh sad"
@@ -46,19 +46,21 @@ const EventsList = () => {
           img={"https://picsum.photos/id/1018/1000/600/"}
           place={"Claregalway Community Centre, Claregalway H91 THH4, Ireland"}
           date={"2024"}
-        />
+        /> */}
         {events.map((event, key) => (
           <Event
             key={key}
             name={event.name}
             img={event.img}
+            date={event.date}
             description={event.description}
             place={event.place}
+            eventURL={event.eventURL}
           />
         ))}
     </div>
-    <div>
-      <button type="submit" className="eventBtn">More Events</button>
+    <div className="eventBtnSpace">
+      <a href="https://www.eventbrite.com/" className="eventBtn roboto-serif-ireland" target="_blank" rel="noopener noreferrer">More Events</a>
     </div>
     </>
   );
