ec4239ffbf898fcc7eef8b784e954a522dee207e
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-03-08 18:49:23 +0000

    Image for Carusel added

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -11,42 +11,11 @@ const EventsList = () => {
 
   console.log(events)
 
-  // const events = [
-  //   {
-  //     name: "Claregalway Drama Festival 2024",
-  //     date: "14 - 22 Mar, 2024",
-  //     description:
-  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-  //   },
-  //   {
-  //     name: "St. Patrick’s Day Festival 2024",
-  //     date: "14 - 22 Mar, 2024",
-  //     description:
-  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-  //   },
-  //   {
-  //     name: "Múscailt – Annual Arts Festival",
-  //     date: "14 - 22 Mar, 2024",
-  //     description:
-  //       "Prepare to be enthralled at the Claregalway Drama Festival, running from Thursday, March 14th to Friday, March 22nd, 2024, from 8:00 pm to 10:30 pm nightly. Hosted at the Claregalway...",
-  //     place: "Claregalway Community Centre, Claregalway H91 THH4, Ireland",
-  //   },
-  // ];
-
-  return (
+  if (setEvents.length > 0) {
+    return (
     <>
     <div className="eventContainer">
-        {/* <Event
-          name={"NEW event"}
-          description={
-            "khsadkajs hdkjahd kajhsdkjahsk djhaskdjhaskjdha skjhaskdjh sad"
-          }
-          img={"https://picsum.photos/id/1018/1000/600/"}
-          place={"Claregalway Community Centre, Claregalway H91 THH4, Ireland"}
-          date={"2024"}
-        /> */}
+        
         {events.map((event, key) => (
           <Event
             key={key}
@@ -62,8 +31,13 @@ const EventsList = () => {
     <div className="eventBtnSpace">
       <a href="https://www.eventbrite.com/" className="eventBtn roboto-serif-ireland" target="_blank" rel="noopener noreferrer">More Events</a>
     </div>
-    </>
-  );
+    </>);
+  }else{
+    return(<h3>Data is loading...</h3>);
+  }
+
+  
+  
 };
 
 export default EventsList;
